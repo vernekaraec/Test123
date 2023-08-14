2337f94cf5d1d06ed2f17b70ee2fdb7583b8ca25
Anant Vernekar <vernekar.aec@gmail.com>
2023-08-14 15:58:23 +0530

    message

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!1");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+
+Test t = new Test();
+t.GetHashCode();
